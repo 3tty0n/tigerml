9157cceb1f41a5a2afbe2efe77f80c5743e4e344
Yusuke Izawa <me@yizawa.com>
2023-12-28 21:05:39 +0900

    print ast

--- main.ml
+++ main.ml
@@ -2,8 +2,8 @@ let run filename =
   try
     let ast = Parse.parse filename in
     (* let _ = FindEscape.findEscape ast in *)
+    print_string (Absyn.show_exp ast); print_newline ();
     let exp = Semant.transProg ast in
-    (* print_string (Absyn.show_exp ast); print_newline (); *)
     print_string (Translate.show_exp exp)
   with Semant.SemanticError -> print_endline ("Failed to typecheck \"" ^ filename ^ "\"."); exit 1
 
